e57d3283f667990721f9984aeef0da5b73b0a222
Jacky Zhang <yzhang69@email.wm.edu>
2022-07-13 23:37:59 -0400

    Initial Commit

---
+++ StockIndexDaily/StockIndexDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockIndexDailyApp.swift
+//  StockIndexDaily
+//
+//  Created by Skywalker on 2022/7/13.
+//
+
+import SwiftUI
+
+@main
+struct StockIndexDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
