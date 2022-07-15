adaf511cb35af583ebd9a7e6e29e496427595e8f
Jacky Zhang <yzhang69@email.wm.edu>
2022-07-14 23:49:15 -0400

    Initialize repo

---
+++ StockIndexDaily/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  StockIndexDaily
+//
+//  Created by Skywalker on 2022/7/14.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
---
+++ StockIndexDaily/Views/LandmarkRow.swift
@@ -0,0 +1,33 @@
+//
+//  LandmarkRow.swift
+//  StockIndexDaily
+//
+//  Created by Skywalker on 2022/7/14.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarks[3])
+            LandmarkRow(landmark: landmarks[0])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
